cbd23878c12568d8835be054414a954bec9c704b
liubio0 <liubio0@126.com>
2021-09-23 23:48:40 +0800

    Update MysqlproxyApplicationTests.java

--- Week7/mysqlproxy/src/test/java/mysql/liuzm/homework/mysqlproxy/MysqlproxyApplicationTests.java
+++ Week7/mysqlproxy/src/test/java/mysql/liuzm/homework/mysqlproxy/MysqlproxyApplicationTests.java
@@ -5,26 +5,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MysqlproxyApplicationTests {
 
     @Autowired
     @Qualifier("masterJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate1;
+    protected JdbcTemplate writeJdbcTemplate;
 
     @Autowired
     @Qualifier("slaveJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate2;
+    protected JdbcTemplate readJdbcTemplate;
 
     @Test
-    void contextLoads() throws Exception{
+    void contextLoads() {
         insert();
+        read();
     }
 
-    public void insert() throws Exception {
-        jdbcTemplate1.update("insert into t1(no) values (?)", 1);
-        jdbcTemplate2.update("insert into t1(no) values (?)", 2);
+    public void insert() {
+        writeJdbcTemplate.update("insert into t1 values (?)", 6);
+    }
+
+    public void read(){
+//        List result = readDataSource.query("select * from t1", new RowMapper<Map>() {
+//            @Override
+//            public Map mapRow(ResultSet rs, int rowNum) throws SQLException{
+//                Map row = new HashMap();
+//                row.put(rs.getInt("no"), rs.getInt("no"));
+//                return row;
+//            }});
+        List result = readJdbcTemplate.queryForList("select * from t1");
+        System.out.println(result);
     }
 
 }
